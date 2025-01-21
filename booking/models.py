08f9059c48216a3486634490f20eaba6720f68dd
Mykhailo Vasylkov <vasilkoff.mihail@gmail.com>
2025-01-21 13:21:22 +0000

    Add booking model.

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Reservation(models.Model):
+    STATUS_CHOICES = [
+        (0, 'Pending'),
+        (1, 'Confirmed'),
+        (2, 'Cancelled'),
+    ]
+
+    client = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations')
+    phone_number = models.CharField(max_length=15)  # Adjust max_length as needed
+    date = models.DateField()
+    time = models.TimeField()
+    table_count = models.PositiveIntegerField()
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+
+    def __str__(self):
+        return f'Reservation by {self.client.username} on {self.date} at {self.time}'
