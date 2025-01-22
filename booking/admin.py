03f0e90e814da34e8a9bf7a1e58acf1289f6b767
Mykhailo Vasylkov <vasilkoff.mihail@gmail.com>
2025-01-22 13:45:09 +0000

    Register Reservation model with dispalying, search, filter functionality

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Reservation
 
-# Register your models here.
-admin.site.register(Reservation)
+"""
+Register Reservation model in admin panel.
+Add how reservations are displaying.
+Add search functionality.
+Add filter functionality.
+"""
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+
+    list_display = ('client', 'phone_number', 'date', 'time', 'table_count', 'status')
+    search_fields = ['client', 'phone_number', 'comment']
+    list_filter = ('date', 'status')
