889e8adbec39c678748cfb59995aec75f21cad9c
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-02-13 10:22:41 +0000

    Register Review model in admin panel.

--- contact/admin.py
+++ contact/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import Review
+
+"""
+Register Review model in admin panel.
+Add how reviews are displaying.
+Add search functionality.
+Add filter functionality.
+"""
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = ('author', 'rating', 'created_on', 'approved',)
+    search_fields = ['author', 'body',]
+    list_filter = ('rating', 'approved',)
