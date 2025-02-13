4145dea696d6362da87eda6c91330d4465396520
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-02-13 11:07:30 +0000

    Add views for contact app

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import Review
+from .forms import ReviewForm
 
-# Create your views here.
+def review_create(request):
+    reviews = Review.objects.filter(status=1).order_by("-created_on")
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.author = request.user
+            review.save()
+            messages.success(request, 'Review submitted and awaiting approval')
+        else:
+            messages.error(request, 'Error adding review!')
+    else:
+        form = ReviewForm()
+
+    return render(request, 'contact/contact_us.html', {
+        'form': form,
+        'reviews': reviews,
+    })
