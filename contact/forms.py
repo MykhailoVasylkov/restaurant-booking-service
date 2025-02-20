c9044a7c716c82ea45af2ec22d17c664044da748
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-02-20 11:23:29 +0000

    Add tests for Review model

--- contact/forms.py
+++ contact/forms.py
@@ -6,7 +6,7 @@ class ReviewForm(forms.ModelForm):
     A form for creating a single review.
     """
     rating = forms.IntegerField(min_value=1, max_value=5, required=True)
-    
+ 
     class Meta:
         model = Review
         fields = ['author', 'rating', 'body',]
