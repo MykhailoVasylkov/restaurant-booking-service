8b70b35b71ba317d7fd7e96a85f55b0b331d7a64
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-02-21 22:17:01 +0000

    Add test_views for home app

---
+++ home/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class HomePageViewTests(TestCase):
+    def test_home_page_view(self):
+        """Test that the home page returns a 200 status code and uses the correct template."""
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/index.html")
