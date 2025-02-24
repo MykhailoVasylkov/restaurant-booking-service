25d495be1ea093ede43864c8e34a50763827f794
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-02-24 08:19:14 +0000

    Allow iframe embedding

--- napoli/settings.py
+++ napoli/settings.py
@@ -30,11 +30,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
     '127.0.0.1', ]
+
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 # Google Maps API Key
 GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
